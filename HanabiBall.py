f59090160e999aa114e72914529f6af8c9e6c804
SoraY677 <ys4080@icloud.com>
2021-08-15 11:58:21 +0900

    hanabiの玉をいい感じに

---
+++ HanabiBall.py
@@ -0,0 +1,17 @@
+import math
+from kivy.graphics import Color
+
+hanabiBall = []
+
+
+def init():
+    # 花火の玉の初期状態を生成
+    global hanabiBall
+    for tate in range(0, 29):
+        hanabiBall.append([])
+        for yoko in range(0, 29):
+            z = math.sqrt(((14 - tate)**2) + ((14 - yoko)**2))
+            if z <= 15:
+                hanabiBall[tate].append(None)
+            else:
+                hanabiBall[tate].append([0.2, 0.2, 0.2])
--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
+from functools import partial
+import HanabiBall
+from kivy.graphics import Color
+
 import math
 
 from AnimationLib import HanabiAnim
+
+_color = [0.2, 0.2, 0.2, 1]
 
 
 class EditorPanel(BoxLayout):  # ユーザーインタフェースを初期化するクラス
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        layout = BoxLayout()
+        HanabiBall.init()
         layout = GridLayout(cols=29)
         layout.size_hint = (1, 1)
 
@@ -25,6 +31,15 @@ class EditorPanel(BoxLayout):  # ユーザーインタフェースを初期化�
                 z = math.sqrt(((14 - tate)**2) + ((14 - yoko)**2))
                 if z <= 15:
                     buttonlist[tate].append(Button())
+                    buttonlist[tate][yoko].background_color = [0.2, 0.2, 0.2, 1]
+
+                    def callback(num, instance):
+                        global _color
+                        instance.background_color = _color
+                        HanabiBall.hanabiBall[num[0]][num[1]] = [_color[0], _color[1], _color[2]]
+                        print(HanabiBall.hanabiBall[num[0]][num[1]])
+
+                    buttonlist[tate][yoko].bind(on_press=partial(callback, [tate, yoko]))
                     layout.add_widget(buttonlist[tate][yoko])
                 else:
                     buttonlist[tate].append(None)
@@ -32,6 +47,23 @@ class EditorPanel(BoxLayout):  # ユーザーインタフェースを初期化�
         self.ids['"edit_canvas"'].add_widget(layout)
 
         HanabiAnim.init(canvas=self.ids['"simulation_canvas"'].canvas, trigger=self.ids['"simulation_start"'])
+
+    def choose_color(self, colornum):
+        global _color
+        _color = (
+            (0.9, 0, 0, 1),
+            (0.94, 0.45, 0, 1),
+            (1, 0.98, 0, 1),
+            (0.58, 0.7, 0.2, 1),
+            (0, 0.8, 0.26, 1),
+            (0, 0.68, 0.9, 1),
+            (0, 0.6, 0.74, 1),
+            (0.2, 0.2, 0.7, 1),
+            (0.6, 0.08, 0.6, 1),
+            (0.8, 0, 0.53, 1),
+            (1, 1, 1, 1),
+            (0, 0, 0, 1)
+        )[colornum - 1]
 
 
 class EditorApp(App):  # アプリケーションのロジックを記述するクラス
